01cc930786fe41c43908e15fb812f33447299767
Victor Shevchuk <v.shevchuk@viseven.com>
2021-09-22 09:19:54 +0400

    css modules

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,44 +1,43 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
 
 import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import styles from './CourseInput.module.css';
 
-const setColorDependingOnValidity = (props) =>
-  props.invalid ? '#dc3545' : 'black';
-const setBorderColorDependingOnValidity = (props) =>
-  props.invalid ? '#dc3545' : '#ccc';
-const setBoxShadowDependingOnValidity = (props) =>
-  props.invalid && '0 0 0 0.25rem rgb(220 53 69 / 25%)';
+// const setColorDependingOnValidity = (props) =>
+//   props.invalid ? '#dc3545' : 'black';
+// const setBorderColorDependingOnValidity = (props) =>
+//   props.invalid ? '#dc3545' : '#ccc';
+// const setBoxShadowDependingOnValidity = (props) =>
+//   props.invalid && '0 0 0 0.25rem rgb(220 53 69 / 25%)';
 
-const FormControl = styled.div`
-   {
-    margin: 0.5rem 0;
-  }
+// const FormControl = styled.div`
+//    {
+//     margin: 0.5rem 0;
+//   }
 
-  & label {
-    font-weight: bold;
-    display: block;
-    margin-bottom: 0.5rem;
-    color: ${setColorDependingOnValidity};
-  }
+//   & label {
+//     font-weight: bold;
+//     display: block;
+//     margin-bottom: 0.5rem;
+//     color: ${setColorDependingOnValidity};
+//   }
 
-  & input {
-    display: block;
-    width: 100%;
-    border: 1px solid ${setBorderColorDependingOnValidity};
-    font: inherit;
-    line-height: 1.5rem;
-    padding: 0 0.25rem;
-    box-shadow: ${setBoxShadowDependingOnValidity};
-  }
+//   & input {
+//     display: block;
+//     width: 100%;
+//     border: 1px solid ${setBorderColorDependingOnValidity};
+//     font: inherit;
+//     line-height: 1.5rem;
+//     padding: 0 0.25rem;
+//     box-shadow: ${setBoxShadowDependingOnValidity};
+//   }
 
-  & input:focus {
-    outline: none;
-    background: #fad0ec;
-    border-color: #8b005d;
-  }
-`;
+//   & input:focus {
+//     outline: none;
+//     background: #fad0ec;
+//     border-color: #8b005d;
+//   }
+// `;
 
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState('');
@@ -63,10 +62,12 @@ const CourseInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <FormControl invalid={!isValid}>
+      <div
+        className={`${styles['form-control']} ${!isValid && styles.invalid}`}
+      >
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
-      </FormControl>
+      </div>
       <Button type="submit">Add Goal</Button>
     </form>
   );
